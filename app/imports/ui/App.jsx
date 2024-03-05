931a1be3e4a1fe816158b13d5af13e0a70e0cf3b
Hau'olinalani O'Brien <ulapulelehua@gmail.com>
2024-03-04 17:02:43 -1000

    Section 3

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
-import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import { TaskForm } from './TaskForm';
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => {
+    const fetchedTasks = TasksCollection.find({}).fetch();
+    // Add createdAt field to each task object
+    return fetchedTasks.map(task => ({ ...task, createdAt: new Date() }));
+  });
 
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
+
+      <TaskForm/>
 
       <ul>
         { tasks.map(task => <Task key={ task._id } task={ task }/>) }
@@ -16,4 +23,6 @@ export const App = () => {
     </div>
   );
 };
+
+
 
